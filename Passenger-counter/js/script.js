beeacbcdb58d6f83c15a00ae4e49a42452242d43
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-14 23:36:01 +0600

    Some remove file

--- Passenger-counter/js/script.js
+++ Passenger-counter/js/script.js
@@ -2,10 +2,6 @@ const count = document.getElementById('count');
 const btnIncrease = document.getElementById('incrise');
 const btnDecrise = document.getElementById('decrise');
 let countValue = 0;
-if(countValue <= 0){
-  countValue.innerText = "Sorry set posative number";
-}
-else{
   btnIncrease.addEventListener('click', () => {
     countValue++;
     count.innerText = countValue;
@@ -14,4 +10,3 @@ else{
     countValue--;
       count.innerText = countValue;
   });
-}

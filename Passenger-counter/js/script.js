e1658d4d238a02f373c1131ddd7319bdbedb1c48
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-14 23:07:41 +0600

    Add condition with value

--- Passenger-counter/js/script.js
+++ Passenger-counter/js/script.js
@@ -2,15 +2,16 @@ const count = document.getElementById('count');
 const btnIncrease = document.getElementById('incrise');
 const btnDecrise = document.getElementById('decrise');
 let countValue = 0;
-btnIncrease.addEventListener('click', () => {
-  countValue++;
-  count.innerText = countValue;
-});
-btnDecrise.addEventListener('click', () => {
-  if(countValue <= 0){
-    countValue.innerText = 'Add Number no negative number';
-  }else{
-    countValue--;
+if(countValue <= 0){
+  countValue.innerText = "Sorry set posative number";
+}
+else{
+  btnIncrease.addEventListener('click', () => {
+    countValue++;
     count.innerText = countValue;
-  }
-});
+  });
+  btnDecrise.addEventListener('click', () => {
+    countValue--;
+      count.innerText = countValue;
+  });
+}

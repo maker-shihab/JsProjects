27ea497136b4c2bf88526ac5d82024ba9f2e17ba
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-14 22:59:41 +0600

    add increase value

--- Passenger-counter/js/script.js
+++ Passenger-counter/js/script.js
@@ -1,3 +1,8 @@
 const count = document.getElementById('count');
 const btnIncrease = document.getElementById('incrise');
 const btnDecrise = document.getElementById('decrise');
+let countValue = 0;
+btnIncrease.addEventListener('click', () => {
+  countValue++;
+  count.innerText = countValue;
+})

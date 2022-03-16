e252071314a0f21292c8eac4f56b36612a4e0af3
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-16 23:43:43 +0600

    Add User
    Add User for show profile

--- github-profile/script.js
+++ github-profile/script.js
@@ -22,6 +22,29 @@ async function getRepos(username) {
     addReposToCard(respData);
 }
 
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${user.name}" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul class="info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+                <div id="repos"></div>
+            </div>
+        </div>
+    `;
+
+    main.innerHTML = cardHTML;
+}
+
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
